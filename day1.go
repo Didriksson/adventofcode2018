f8f35139c1e9aceefadece10319d085e702f0268
Mattias Didriksson <mattias.didriksson@gmail.com>
2018-12-01 21:02:09 +0100

    Day 1 complete! New language and you can probably tell looking at the code.

---
+++ day1.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	currentFreq := 0
+	seenFreqs := make(map[int]bool)
+	seenFreqs[0] = true
+	for {
+		currentFreq = iterateList(currentFreq, seenFreqs)
+	}
+}
+
+func iterateList(currentFreq int, seenFreqs map[int]bool) int {
+	file, err := os.Open("day1.txt")
+
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		value, _ := strconv.Atoi(scanner.Text())
+		currentFreq += value
+		_, exist := seenFreqs[currentFreq]
+		if exist {
+			fmt.Println("Hittat upprepad frekvens: ", currentFreq)
+			os.Exit(0)
+		}
+		seenFreqs[currentFreq] = true
+		fmt.Println(currentFreq)
+	}
+	return currentFreq
+}
