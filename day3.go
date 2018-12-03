5dbfb886d1e9a2ce58166f3d3862477ffed8f165
Mattias Didriksson <mattias.didriksson@gmail.com>
2018-12-03 21:07:33 +0100

    Day 3. Again not that happy with the results. Tons of loops and redeclarations of the ReadFile-method.

--- day3.go
+++ day3.go
@@ -4,33 +4,88 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	input := ReadFile("day3.txt")
 	var claims = []claim{}
+	fabric := make(map[string][]int)
 
 	for _, line := range input {
 		claims = append(claims, parseClaim(line))
 	}
-	fmt.Println(claims)
-//	fabric := make(map[string]string)
+
+	for _, c := range claims {
+		fabric = applyClaimToFabric(c, fabric)
+	}
+
+	fmt.Println("Answer to part 1:", lookForOverlapping(fabric))
+
+	noOfUniqueCellsPerID := removeDuplicates(fabric)
+	fmt.Println("Answer to part 2: Claim with id - ", lookForMatchingClaim(claims, noOfUniqueCellsPerID).id)
+
 }
 
-func parseClaim(line string) claim{ReadFile("day3.txt")
+func lookForMatchingClaim(claimsList []claim, uniquePerIds map[int]int) claim {
+	for id, numberOf := range uniquePerIds {
+		for _, claim := range claimsList {
+			if claim.id == id {
+				if claim.w*claim.h == numberOf {
+					return claim
+				}
+				break
+			}
+		}
+	}
+	fmt.Println("Could not find the mathing claim")
+	return claim{}
+}
+
+func removeDuplicates(f map[string][]int) map[int]int {
+	var uniqueCells = make(map[int]int)
+	for _, v := range f {
+		if len(v) == 1 {
+			uniqueCells[v[0]] = uniqueCells[v[0]] + 1
+		}
+	}
+	return uniqueCells
+}
+
+func lookForOverlapping(f map[string][]int) int {
+	var overlapping int
+	for _, id := range f {
+		if len(id) > 1 {
+			overlapping++
+		}
+	}
+	return overlapping
+}
+
+func applyClaimToFabric(c claim, f map[string][]int) map[string][]int {
+	for y := c.y; y < c.y+c.h; y++ {
+		for x := c.x; x < c.x+c.w; x++ {
+			cord := strings.Join([]string{strconv.Itoa(x), strconv.Itoa(y)}, ",")
+			f[cord] = append(f[cord], c.id)
+		}
+	}
+	return f
+}
+
+func parseClaim(line string) claim {
+	ReadFile("day3.txt")
 	s := strings.Split(line, " ")
 	id := parseToInt(string(s[0][1:]))
-	
+
 	cords := strings.Split(s[2], ",")
 	x := parseToInt(cords[0])
 	y := parseToInt(strings.TrimSuffix(cords[1], ":"))
-	
+
 	w := parseToInt(strings.Split(s[3], "x")[0])
 	h := parseToInt(strings.Split(s[3], "x")[1])
 
-	return claim{id, x ,y ,w, h}
+	return claim{id, x, y, w, h}
 }
 
 func parseToInt(value string) int {
@@ -53,10 +108,10 @@ func ReadFile(path string) []string {
 	return lines
 }
 
-type claim struct{
+type claim struct {
 	id int
-	x int
-	y int
-	w int
-	h int
+	x  int
+	y  int
+	w  int
+	h  int
 }
