0b3378c4107d38fb1ddb5d3f2fd048836c9d1cf0
Mattias Didriksson <mattias.didriksson@lantmateriet.se>
2018-12-05 12:00:16 +0100

    En brute-variant av del 1.

--- day5.py
+++ day5.py
@@ -1,8 +1,13 @@
 def taBortBokstavsskillnad(line):
-    for index, item in enumerate(line):
-        if index < (len(line) - 1):
-            if item != line[index+1] and item.lower() == line[index + 1].lower():
-                return taBortBokstavsskillnad(line[:index] + line[index+2:])
+    rensat = True
+    while rensat:
+        rensat = False
+        for index, item in enumerate(line):
+            if index < (len(line) - 1):
+                if item != line[index+1] and item.lower() == line[index + 1].lower():
+                    line = line[:index] + line[index+2:]
+                    rensat = True
+                    break
     return line
 
 if __name__ == '__main__':
